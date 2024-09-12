1612f9f509e1b532a8b948d055279f629b840b37
MonicaDocByte <monica.manghnani@sysdig.com>
2024-09-12 17:59:54 +0100

    Test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico', 
 
   // Set the production url of your site here
-  url: 'https://docs-sysdig.github.io',
+  url: 'https://sysdig-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/sysdig-docs/',
