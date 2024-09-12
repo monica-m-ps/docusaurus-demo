253d32d4f5688a0f6be1c6129bf4b3cdaef7f4c1
MonicaDocByte <monica.manghnani@sysdig.com>
2024-09-12 17:58:45 +0100

    Test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sysdig', // Usually your GitHub org/user name.
-  projectName: 'main', // Usually your repo name.
+  projectName: 'sysdig-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
